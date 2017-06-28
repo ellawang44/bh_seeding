38efc97d7b8aa4c11cb640dffb9ec984ea43a042
Xi Wang <u6083620@anu.edu.au>
2017-06-28 10:06:17 +1000

    moved the pattern matching for init.item in trace.py around to make it slightly more efficient, it won't pattern match after every single match now, it'll just do it for the galaxy at the start.

--- trace.py
+++ trace.py
@@ -31,20 +31,22 @@ def retrieve():
 
     for i in range(0, len(keys) - 1):
         for galaxy in read.galaxy_data[keys[i]]:
+            # decide what item to obtain histogram data for
+            if init.item == 'stellar':
+                item = galaxy[6]
+            elif init.item == 'dark matter':
+                item = galaxy[7]
+            elif init.item == 'black hole':
+                item = galaxy[8]
+            else:
+                item = key[i][0]
             for prev_galaxy in read.galaxy_data[keys[i+1]]:
                 # 1. check that the previous galaxy is below the threshold mass - if it isn't, it already isn't what we're looking for
                 # 2. find the pre-image of the galaxy that you want
                 # 3. check that it has just passed the threshold mass
                 # 1 and 3 can be done together, but this makes the code run slightly faster (I think)
                 if prev_galaxy[var] < obj and galaxy[2] == prev_galaxy[0] and galaxy[var] > obj:
-                    if init.item == 'redshift':
-                        objects.append(key[i][0])
-                    if init.item == 'stellar':
-                        objects.append(galaxy[6])
-                    if init.item == 'dark matter':
-                        objects.append(galaxy[7])
-                    if init.item == 'black hole':
-                        objects.append(galaxy[8])
+                    objects.append(item)
                     if init.print_file == True:
                         f.write(str(int(keys[i][1])) + ' \t ' + str(int(galaxy[0])) + ' \n')
     if objects != []:
