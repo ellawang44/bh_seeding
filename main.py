15d6ea6f52f2777523804b1cfdb0608ac0f378cf
Xi Wang <u6083620@anu.edu.au>
2017-07-31 17:04:37 +1000

    removed unneeded files, also updated parser help comments on main.py.

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import read
 import trace
 import redshift
 import snapshot
-import pres
 import pylab
 import init
-from optparse import OptionParser, OptionGroup
+from optparse import OptionParser
 
 parser = OptionParser()
 
@@ -13,41 +12,43 @@ parser = OptionParser()
 parser.add_option('-f', '--file', type = 'string', dest = 'file', help = 'Determines the input file. Can also parse galaxy data.')
 
 # data for a specific redshift/snapshot
-parser.add_option('-z', '--redshift', type = 'float', dest = 'redshift', help = 'takes the data for the redshift closest to the given redshift. Needs to be used in conjuncture with "--data". The bin size of the histogram can be changed with "--bin", the default is 100.')
+parser.add_option('-z', '--redshift', type = 'float', dest = 'redshift', help = 'Takes the data for the redshift closest to the given redshift. Needs to be used in conjuncture with "--data". The bin size of the histogram can be changed with "--bin", the default is 100.')
 
-parser.add_option('-s', '--snapshot', type = 'int', dest = 'snapshot', help = 'takes the data for the specific snapshot. Needs to be used in conjuncture with "--data". The bin size of the histogram can be changed with "--bin", the default is 100.')
+parser.add_option('-s', '--snapshot', type = 'int', dest = 'snapshot', help = 'Takes the data for the specific snapshot. Needs to be used in conjuncture with "--data". The bin size of the histogram can be changed with "--bin", the default is 100.')
 
 # takes out the required data type
 parser.add_option('--data', type = 'string', dest = 'data', help = 'Determines what data to pick from the galaxy. The possible inputs are "redshift", "xcoord", "ycoord", "zcoord", "stellar", "dark matter" and "black hole".')
 
 # sets the threshold mass
-parser.add_option('-m', '--stellar', type = 'float', dest = 'stellar', help = 'Trace galaxy merges and return the redshift when a particular galaxys stellar mass first goes above the input mass.')
+parser.add_option('-m', '--stellar', type = 'float', dest = 'stellar', help = 'Sets the threshold stellar mass, galaxies that just pass above that threshold mass will be recorded. Use "--data" to determine what aspect of those galaxies to histogram. Use "--downsizing" to plot the redshift at which the galaxies first cross over the threshold vs the aspect of the galaxy selected by the data tag. Use "--txt" to output a txt file containing the galaxy number of the galaxies that just pass above the threshold mass and the snapshot at which it occurs.')
 
-parser.add_option('-d', '--darkmatter', type = 'float', dest = 'darkmatter', help = 'If used alone, will trace galaxy merges and return the redshift when a particular galaxys dark matter mass first goes above the input mass. If used in conjuncture with --range: for the given dark matter mass, takes all the haloes that has dark matter mass in conjuncturein the given range around the specified mass and returns the redshift at which they occur and the number of times it occurs at that redshift. If used in conjuncture --snapshot or --redshift: returns the distribution of dark matter halo masses for a the given snapshot/redshift.')
+parser.add_option('-d', '--darkmatter', type = 'float', dest = 'darkmatter', help = 'Sets the threshold dark matter halo mass, galaxies that just pass above that threshold mass will be recorded. Use "--data" to determine what aspect of those galaxies to histogram. Use "--downsizing" to plot the redshift at which the galaxies first cross over the threshold vs the aspect of the galaxy selected by the data tag. Use "--txt" to output a txt file containing the galaxy number of the galaxies that just pass above the threshold mass and the snapshot at which it occurs.')
 
-parser.add_option('-b', '--blackhole', type = 'float', dest = 'blackhole', help = 'If used alone, will trace galaxy merges and return the redshift when a particular galaxys black hole mass first goes above the input mass. If used in conjuncture with --range: for the given black hole mass, takes all the haloes that has black hole mass in conjuncturein the given range around the specified mass and returns the redshift at which they occur and the number of times it occurs at that redshift. If used in conjuncture --snapshot or --redshift: returns the distribution of black hole masses for a the given snapshot/redshift.')
+parser.add_option('-b', '--blackhole', type = 'float', dest = 'blackhole', help = 'Sets the threshold black hole mass, galaxies that just pass above that threshold mass will be recorded. Use "--data" to determine what aspect of those galaxies to histogram. Use "--downsizing" to plot the redshift at which the galaxies first cross over the threshold vs the aspect of the galaxy selected by the data tag. Use "--txt" to output a txt file containing the galaxy number of the galaxies that just pass above the threshold mass and the snapshot at which it occurs.')
 
 # takes the present day image of the galaxies of interest
-parser.add_option('--downsizing', action = 'store_true', dest = 'downsizing', help = 'Takes the selected data and outputs a scatter plot of the redshift along the x-axis and the data on the y-axis.')
+parser.add_option('--downsizing', action = 'store_true', dest = 'downsizing', help = 'Takes the selected data and outputs a scatter plot of the redshift along the x-axis and the data on the y-axis. Used together with "--data" and either "--stellar", "--darkmatter" or "--blackhole".')
 
 # print file containing galaxies of interest
-parser.add_option('--txt', action = 'store_true', dest = 'txt', help = 'outputs a file containing the galaxy number of galaxies that cross over the threshold and the snapshot number when they cross over.')
+parser.add_option('--txt', action = 'store_true', dest = 'txt', help = 'outputs a file containing the galaxy number of galaxies that cross over the threshold and the snapshot number when they cross over. Used together with "--data" and either "--stellar", "--darkmatter" or "--blackhole".')
 
 # number of bins in histogram
-parser.add_option('--bin', type = 'int', dest = 'bin', help = 'Number of bins drawn on the histgram for data for a single snapshot or redshift.')
+parser.add_option('--bin', type = 'int', dest = 'bin', help = 'Determines the number of bins drawn on the histgram.')
 
 if __name__ == '__main__':
     (options, args) = parser.parse_args()
 
+    # sets the correct file to read in
     if options.file:
         init.file = options.file
 
     read_data = read.FileData(init.file_name)
     binnum = 100 # default number of bins
-    # used for all histograms so it's up here
+    # changes the bin sizes used in histograms
     if options.bin:
         binnum = options.bin
 
+    # sets the variable and name associated with the wanted data
     var, name = { 'redshift' : (0, 'redshift'),
                  'xcoord' : (4, 'x-coordinates'),
                  'ycoord' : (5, 'y-coordinates'),
@@ -78,7 +79,7 @@ if __name__ == '__main__':
         pylab.ylabel('number')
         pylab.show()
 
-    # prints file output
+    # if txt output is wanted
     if options.txt:
         init.print_file = True
 
@@ -102,9 +103,10 @@ if __name__ == '__main__':
             res = [g[0][var] for g in galaxies2]
         else:
             res = [g[1][var] for g in galaxies2]
+        # produces scatter plot
         if options.downsizing:
             redshift = [g[0].redshift for g in galaxies2]
-            # if black holes, need to filter out -inf
+            # if black holes, filter out -inf
             if var == 9:
                 filt = [g for g in zip(redshift, res) if g[1] != float('-inf')]
                 redshift = [g[0] for g in filt]
@@ -114,7 +116,9 @@ if __name__ == '__main__':
             pylab.xlabel('redshift')
             pylab.ylabel(name)
             pylab.show()
+        # produces histogram
         else:
+            # if black holes, filter out -inf
             if var == 9:
                 res = [g for g in res if g != float('-inf')]
             pylab.hist(res, bins = binnum)
